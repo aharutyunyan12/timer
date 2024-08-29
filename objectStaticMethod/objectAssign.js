53077466eb742669bf8e6c31c137e39e50506e7c
Anna Harutyunyan <aharutyunyan745@gmail.com>
2024-08-29 19:48:57 +0400

    feat: add Object.assign array

--- objectStaticMethod/objectAssign.js
+++ objectStaticMethod/objectAssign.js
@@ -17,3 +17,10 @@ const res = Object.assign(obj, obj2, obj3)
 //obj = { ...obj, ...obj2, ...obj3 }
 
 console.log(res)
+
+const arr = [1, 2, 3]
+const arr2 = [3, 4, 5]
+
+Object.assign(arr, arr2)
+
+console.log(arr)
